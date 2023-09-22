2c66a47c83f4d1b28ff78dcd065b28588adf17eb
ConstanzaOyarzo <coyarzod0@gmail.com>
2023-09-22 18:42:24 -0300

    añadir materias

--- taller 1 2023/calculadora.js
+++ taller 1 2023/calculadora.js
@@ -1,24 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const calculateButton = document.getElementById("calculate");
-    const resultParagraph = document.getElementById("result");
+// 1.- Funcion
+// 2.- Iniciar objetos
+// 3.- Escuchar eventos
 
-    calculateButton.addEventListener("click", function () {
-        // Obtener las notas y créditos de las materias
-        const grade1 = parseFloat(document.getElementById("grade1").value);
-        const credit1 = parseFloat(document.getElementById("credit1").value);
-        const grade2 = parseFloat(document.getElementById("grade2").value);
-        const credit2 = parseFloat(document.getElementById("credit2").value);
-        const grade3 = parseFloat(document.getElementById("grade3").value);
-        const credit3 = parseFloat(document.getElementById("credit3").value);
+function editariHTML(objeto, texto) {
+    objeto.innerHTML = texto;
+}
 
-        // Calcular el promedio ponderado
-        const weightedAverage = ((grade1 * credit1) + (grade2 * credit2) + (grade3 * credit3)) / (credit1 + credit2 + credit3);
+const boton = document.querySelector('#agregarMateria');
+const divMaterias = document.querySelector('#materias');
 
-        // Mostrar el resultado
-        if (!isNaN(weightedAverage)) {
-            resultParagraph.textContent = `Tu promedio ponderado es: ${weightedAverage.toFixed(2)}`;
-        } else {
-            resultParagraph.textContent = "Por favor, ingresa todas las notas y créditos.";
-        }
-    });
-});
+
+boton.addEventListener("click", function() {
+    const input = document.querySelector('#nombreMateria').value;
+    const nuevop = document.createElement("p");
+    divMaterias.appendChild(nuevop);
+    nuevop.innerText = input;
+    editariHTML(divMaterias, input);
+})
