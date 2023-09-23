08f547f72b170e9281fa8553bc70a4ebdb742756
Rodrigo Carmonaa <rcarmona2023@alu.uct.cl>
2023-09-22 21:35:29 -0300

    actualizacion

--- taller 1 2023/calendario.js
+++ taller 1 2023/calendario.js
@@ -1,90 +1,90 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const currentMonthDisplay = document.getElementById("current-month");
-    const calendarBody = document.getElementById("calendar-body");
-    const eventDateInput = document.getElementById("event-date");
-    const eventDescriptionInput = document.getElementById("event-description");
-    const addEventButton = document.getElementById("add-event");
-    const eventList = document.getElementById("event-list");
-  
-    let currentDate = new Date();
-    let selectedDate = currentDate;
-  
-    function generateCalendar(year, month) {
-        const firstDayOfMonth = new Date(year, month, 1);
-        const lastDayOfMonth = new Date(year, month + 1, 0);
-        const daysInMonth = lastDayOfMonth.getDate();
-        const startingDay = firstDayOfMonth.getDay();
-        calendarBody.innerHTML = "";
-      
-        currentMonthDisplay.textContent = new Intl.DateTimeFormat("es-ES", {
-          year: "numeric",
-          month: "long",
-        }).format(firstDayOfMonth);
-      
-        // Crea filas para las semanas en el calendario
-        let currentRow = document.createElement("tr");
-        calendarBody.appendChild(currentRow);
-      
-        // Agrega celdas para los días de la semana
-        for (let i = 0; i < startingDay; i++) {
-          const dayCell = document.createElement("td");
-          dayCell.textContent = "";
-          currentRow.appendChild(dayCell);
-        }
-      
-        // Agrega celdas para los días del mes
-        for (let day = 1; day <= daysInMonth; day++) {
-          if (currentRow.children.length === 7) {
-            currentRow = document.createElement("tr");
-            calendarBody.appendChild(currentRow);
+document.addEventListener("DOMContentLoaded", function() {
+  const calendar = document.getElementById("calendar");
+  const taskInput = document.getElementById("task");
+  const addTaskButton = document.getElementById("add-task");
+  const taskList = document.getElementById("tasks");
+
+  // Obtener la fecha actual
+  const today = new Date();
+  let currentMonth = today.getMonth();
+  let currentYear = today.getFullYear();
+
+  // Array de nombres de los días de la semana
+  const daysOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+
+  // Función para obtener el nombre del día de la semana
+  function getDayOfWeek(date) {
+      return daysOfWeek[date.getDay()];
+  }
+
+  // Función para generar el calendario
+  function generateCalendar(month, year) {
+      const firstDay = new Date(year, month, 1);
+      const lastDay = new Date(year, month + 1, 0);
+      const daysInMonth = lastDay.getDate();
+      const startingDay = firstDay.getDay();
+
+      // Crear una tabla para el calendario
+      let html = "<table><tr><th>Domingo</th><th>Lunes</th><th>Martes</th><th>Miércoles</th><th>Jueves</th><th>Viernes</th><th>Sábado</th></tr><tr>";
+
+      // Rellenar los días vacíos al principio del mes
+      for (let i = 0; i < startingDay; i++) {
+          html += "<td></td>";
+      }
+
+      // Rellenar los días del mes
+      for (let day = 1; day <= daysInMonth; day++) {
+          const date = new Date(year, month, day);
+          const dayOfWeek = getDayOfWeek(date);
+          html += `<td class="calendar-day" data-day="${dayOfWeek}, ${day}">${day}</td>`;
+
+          // Pasar a la siguiente fila al final de la semana
+          if ((day + startingDay) % 7 === 0) {
+              html += "</tr><tr>";
           }
-      
-          const dayCell = document.createElement("td");
-          dayCell.textContent = day;
-          dayCell.addEventListener("click", () => {
-            selectedDate = new Date(year, month, day);
-            eventDateInput.valueAsDate = selectedDate;
+      }
+
+      // Cerrar la tabla
+      html += "</tr></table>";
+
+      calendar.innerHTML = html;
+
+      // Agregar evento al hacer clic en un día
+      const dayCells = document.querySelectorAll(".calendar-day");
+      dayCells.forEach(cell => {
+          cell.addEventListener("click", () => {
+              const taskText = taskInput.value.trim();
+              if (taskText !== "") {
+                  const dayAndDate = cell.getAttribute("data-day");
+                  const taskItem = document.createElement("li");
+                  taskItem.textContent = `${dayAndDate}: ${taskText}`;
+                  taskList.appendChild(taskItem);
+                  taskInput.value = "";
+              }
           });
-          currentRow.appendChild(dayCell);
-        }
+      });
+  }
+
+  // Generar el calendario inicial
+  generateCalendar(currentMonth, currentYear);
+
+  // Cambiar al mes anterior
+  document.getElementById("prev-month").addEventListener("click", () => {
+      currentMonth--;
+      if (currentMonth < 0) {
+          currentMonth = 11;
+          currentYear--;
       }
-      
-  
-    function addEvent() {
-      const eventDate = eventDateInput.value;
-      const eventDescription = eventDescriptionInput.value;
-  
-      if (eventDate && eventDescription) {
-        const eventListItem = document.createElement("li");
-        eventListItem.textContent = `${eventDate}: ${eventDescription}`;
-        eventList.appendChild(eventListItem);
-  
-        eventDateInput.value = "";
-        eventDescriptionInput.value = "";
-      } else {
-        alert("Por favor, complete la fecha y la descripción del evento.");
-      }
-    }
-  
-    function updateCalendar() {
-      const year = selectedDate.getFullYear();
-      const month = selectedDate.getMonth();
-      generateCalendar(year, month);
-    }
-  
-    addEventButton.addEventListener("click", addEvent);
-  
-    document.getElementById("prev-month").addEventListener("click", () => {
-      selectedDate.setMonth(selectedDate.getMonth() - 1);
-      updateCalendar();
-    });
-  
-    document.getElementById("next-month").addEventListener("click", () => {
-      selectedDate.setMonth(selectedDate.getMonth() + 1);
-      updateCalendar();
-    });
-  
-    // Generar el calendario inicial
-    generateCalendar(currentDate.getFullYear(), currentDate.getMonth());
+      generateCalendar(currentMonth, currentYear);
   });
-  
+
+  // Cambiar al mes siguiente
+  document.getElementById("next-month").addEventListener("click", () => {
+      currentMonth++;
+      if (currentMonth > 11) {
+          currentMonth = 0;
+          currentYear++;
+      }
+      generateCalendar(currentMonth, currentYear);
+  });
+});
