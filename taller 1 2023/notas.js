b6101a9bad79d034e23572dc7e089a05b949f67c
ConstanzaOyarzo <coyarzod0@gmail.com>
2023-09-22 21:17:55 -0300

    notas y porcentajes

--- taller 1 2023/notas.js
+++ taller 1 2023/notas.js
@@ -2,6 +2,7 @@
 
 const nNotas = document.querySelector('#nNotas')
 const divNotas = document.querySelector('#divNotas')
+const btn_notas = document.querySelector('#btn_nota')
 
 nNotas.addEventListener('input', function () {
     const numNotas = parseInt(nNotas.value);
@@ -13,5 +14,20 @@ nNotas.addEventListener('input', function () {
         const notaText = document.createElement('p');
         notaText.textContent = `Nota ${i}`;
         divNotas.appendChild(notaText);
+    
+        //
+        const notasInput = document.createElement('input');
+        notasInput.type = 'number';
+        notasInput.placeholder = 'Nota'
+        divNotas.appendChild(notasInput);
+        //
+        const porcentajeInput = document.createElement('input');
+        porcentajeInput.type = 'number';
+        porcentajeInput.placeholder = 'Porcentaje'
+        divNotas.appendChild(porcentajeInput);
     }
 });
+
+// btn_notas.addEventListener('click', function () {
+
+// });
