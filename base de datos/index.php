e73ed7f02464b00ed0939c7349d883eac22b01be
thegavst <gvillanueva2018@alu.uct.cl>
2023-09-26 20:25:40 -0300

    Update index.php

--- base de datos/index.php
+++ base de datos/index.php
@@ -42,7 +42,7 @@
             <a href="index.html">Inicio</a>
             <a href="Asistencia.html">Asistencia</a>
             <a href="calendario.html">Calendario Evaluaciones</a>
-            <a href="../taller 1 2023/horario.html">Horario </a>
+            <a href="#">Horario </a>
             <a href="../taller 1 2023/notas.html">Promedio Ponderado </a>
         </nav>
     </header>
