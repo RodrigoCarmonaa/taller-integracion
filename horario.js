9fd840e9d55cdb403545f8476bcc2bee03ecb422
yiyo <yiyo19hp@gmail.com>
2023-10-01 20:28:00 -0300

    prueba subida de archivos

--- horario.js
+++ horario.js
@@ -1,5 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tabla = document.querySelector('table');
+    const cambiosRealizados = {};
+
+    // Función para cargar datos desde un archivo CSV
+    function cargarDatosDesdeCSV() {
+        fetch('scraping_cursos (1).csv')
+            .then(response => response.text())
+            .then(data => {
+                const filas = data.split('\n');
+                const encabezados = filas[0].split(',');
+                
+                for (let i = 1; i < filas.length; i++) {
+                    const valores = filas[i].split(',');
+                    const fila = tabla.insertRow(-1);
+
+                    for (let j = 0; j < encabezados.length; j++) {
+                        const celda = fila.insertCell(j);
+                        celda.textContent = valores[j];
+                    }
+                }
+            })
+            .catch(error => {
+                console.error('Error al cargar el archivo CSV:', error);
+            });
+    }
+
+    cargarDatosDesdeCSV();
+
+    // El resto del código (editar y guardar cambios) sigue siendo igual
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tabla = document.querySelector('table');
 
     tabla.addEventListener('click', function (event) {
         const cell = event.target;
