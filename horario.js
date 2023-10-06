4bdeceb00fae4111cad0713ba6d036c22a0535db
yiyo <yiyo19hp@gmail.com>
2023-10-06 01:29:36 -0300

    Horario ligeramente funcional

--- horario.js
+++ horario.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const selector = document.getElementById('selector');
+    const horario = document.getElementById('horario');
 
-    // Función para crear opciones a partir de datos CSV
+    // Función para crear filas de horas
+    function crearFilasDeHoras() {
+        for (let i = 8; i <= 20; i++) { // De 8:00 AM a 8:00 PM (13 horas)
+            const fila = horario.insertRow(-1);
+            const hora = fila.insertCell(0);
+            hora.textContent = `${i}:00 - ${i + 1}:00`;
+
+            for (let j = 1; j <= 5; j++) { // 5 días de la semana (Lunes a Viernes)
+                const celda = fila.insertCell(j);
+                celda.textContent = ''; // Inicialmente, las celdas están vacías
+            }
+        }
+    }
+
+    // Función para cargar opciones desde un archivo CSV
     function cargarOpcionesDesdeCSV() {
         fetch('salas.csv')
             .then(response => response.text())
@@ -20,7 +35,44 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     }
 
-    // Llamar a la función para cargar las opciones desde CSV
+    // Llamar a la función para crear filas de horas al cargar la página
+    crearFilasDeHoras();
+
+    // Llamar a la función para cargar opciones desde CSV al cargar la página
     cargarOpcionesDesdeCSV();
-    
+
+    // Evento change en el selector para insertar selección en la tabla
+    selector.addEventListener('change', function () {
+        const seleccion = selector.value;
+
+        // Solicitar al usuario que elija una hora y un día
+        const horaSeleccionada = prompt('Elija una hora (8-20):');
+        const diaSeleccionado = prompt('Elija un día (Lunes, Martes, Miércoles, Jueves, Viernes):');
+
+        // Validar la hora ingresada
+        const horaNumerica = parseInt(horaSeleccionada);
+        if (horaNumerica >= 8 && horaNumerica <= 20) {
+            // Encontrar la fila correspondiente a la hora
+            const filaHora = horario.rows[horaNumerica - 8 + 1]; // +1 para ajustar por la fila de encabezados
+
+            // Validar el día ingresado
+            const columnaDia = {
+                'Lunes': 1,
+                'Martes': 2,
+                'Miércoles': 3,
+                'Jueves': 4,
+                'Viernes': 5
+            }[diaSeleccionado];
+
+            if (filaHora && columnaDia) {
+                // Insertar la selección en la celda correspondiente
+                const celda = filaHora.cells[columnaDia];
+                celda.textContent = seleccion;
+            } else {
+                alert('Hora o día no válidos. Asegúrese de que la hora esté entre 8 y 20 y el día sea uno de los siguientes: Lunes, Martes, Miércoles, Jueves, Viernes.');
+            }
+        } else {
+            alert('Hora no válida. Ingrese una hora entre 8 y 20.');
+        }
+    });
 });
