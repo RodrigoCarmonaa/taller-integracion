85720f7a5d549a7aff10a93747e7e6d6931fe3ac
yiyo <yiyo19hp@gmail.com>
2023-10-08 19:50:43 -0300

    ahora deja ingresar las horas que uno tiene la clase

--- horario.js
+++ horario.js
@@ -2,23 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const selector = document.getElementById('selector');
     const horario = document.getElementById('horario');
 
-    // Función para crear filas de horas
-// Función para crear filas de horas dentro de un intervalo específico
-function crearFilasDeHoras(inicio, fin) {
-    for (let i = inicio; i <= fin; i++) {
-        const fila = horario.insertRow(-1);
-        const hora = fila.insertCell(0);
-        hora.textContent = `${i}:00  - ${i + 1}:00  `;
+    // Función para crear filas de horas dentro de un intervalo específico
+    function crearFilasDeHoras(inicio, fin) {
+        for (let i = inicio; i <= fin; i++) {
+            const fila = horario.insertRow(-1);
+            const hora = fila.insertCell(0);
+            hora.textContent = `${i}:00  - ${i + 1}:00`;
 
-        for (let j = 1; j <= 5; j++) { // 5 días de la semana (Lunes a Viernes)
-            const celda = fila.insertCell(j);
-            celda.textContent = ''; // Inicialmente, las celdas están vacías
+            for (let j = 1; j <= 5; j++) { // 5 días de la semana (Lunes a Viernes)
+                const celda = fila.insertCell(j);
+                celda.textContent = ''; // Inicialmente, las celdas están vacías
+            }
         }
     }
-}
 
-// Llamar a la función para crear filas de horas dentro de un intervalo específico
-crearFilasDeHoras(8, 19); // Ejemplo: crea filas desde las 8:00 AM hasta las 5:00 PM
+    // Llamar a la función para crear filas de horas dentro de un intervalo específico
+    crearFilasDeHoras(8, 19); // Ejemplo: crea filas desde las 8:00 AM hasta las 6:00 PM
 
     // Función para cargar opciones desde un archivo CSV
     function cargarOpcionesDesdeCSV() {
@@ -39,9 +38,6 @@ crearFilasDeHoras(8, 19); // Ejemplo: crea filas desde las 8:00 AM hasta las 5:0
             });
     }
 
-    // Llamar a la función para crear filas de horas al cargar la página
-    crearFilasDeHoras();
-
     // Llamar a la función para cargar opciones desde CSV al cargar la página
     cargarOpcionesDesdeCSV();
 
@@ -49,16 +45,13 @@ crearFilasDeHoras(8, 19); // Ejemplo: crea filas desde las 8:00 AM hasta las 5:0
     selector.addEventListener('change', function () {
         const seleccion = selector.value;
 
-        // Solicitar al usuario que elija una hora y un día
-        const horaSeleccionada = prompt('Elija una hora (8-20):');
+        // Solicitar al usuario que elija un intervalo de horas y un día
+        const horaInicio = parseInt(prompt('Elija la hora de inicio (8-19):'));
+        const horaFin = parseInt(prompt('Elija la hora de fin (9-20):'));
         const diaSeleccionado = prompt('Elija un día (Lunes, Martes, Miércoles, Jueves, Viernes):');
 
-        // Validar la hora ingresada
-        const horaNumerica = parseInt(horaSeleccionada);
-        if (horaNumerica >= 8 && horaNumerica <= 20) {
-            // Encontrar la fila correspondiente a la hora
-            const filaHora = horario.rows[horaNumerica - 8 + 1]; // +1 para ajustar por la fila de encabezados
-
+        // Validar las horas ingresadas
+        if (horaInicio >= 8 && horaInicio <= 19 && horaFin >= 9 && horaFin <= 20 && horaInicio < horaFin) {
             // Validar el día ingresado
             const columnaDia = {
                 'Lunes': 1,
@@ -68,15 +61,23 @@ crearFilasDeHoras(8, 19); // Ejemplo: crea filas desde las 8:00 AM hasta las 5:0
                 'Viernes': 5
             }[diaSeleccionado];
 
-            if (filaHora && columnaDia) {
-                // Insertar la selección en la celda correspondiente
-                const celda = filaHora.cells[columnaDia];
-                celda.textContent = seleccion;
+            if (columnaDia) {
+                // Llamar a la función para rellenar el intervalo de horas con la selección
+                rellenarIntervaloConSeleccion(horaInicio, horaFin, columnaDia, seleccion);
             } else {
-                alert('Hora o día no válidos. Asegúrese de que la hora esté entre 8 y 20 y el día sea uno de los siguientes: Lunes, Martes, Miércoles, Jueves, Viernes.');
+                alert('Día no válido. Asegúrese de que el día sea uno de los siguientes: Lunes, Martes, Miércoles, Jueves, Viernes.');
             }
         } else {
-            alert('Hora no válida. Ingrese una hora entre 8 y 20.');
+            alert('Horas no válidas. Asegúrese de que la hora de inicio esté entre 8 y 19, la hora de fin esté entre 9 y 20, y la hora de inicio sea anterior a la hora de fin.');
         }
     });
+
+    // Función para rellenar un intervalo de horas con la selección
+    function rellenarIntervaloConSeleccion(inicio, fin, columnaDia, seleccion) {
+        for (let i = inicio; i <= fin; i++) {
+            const filaHora = horario.rows[i - 8 + 1]; // +1 para ajustar por la fila de encabezados
+            const celda = filaHora.cells[columnaDia];
+            celda.textContent = seleccion;
+        }
+    }
 });
